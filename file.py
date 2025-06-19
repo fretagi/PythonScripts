988c922d5452247fff9aaaf7012d8c7ff8e35588
fretagi <fernando.retagi@gmail.com>
2025-06-19 18:39:25 +0100

    Add first set of scripts for reading files

--- file.py
+++ file.py
@@ -1,3 +1,10 @@
-file = open('fernando.txt', 'r')
+file = open('fernando.txt', 'r') # read mode
+#file = open('fernando.txt', 'w') # write mode
+#file = open('fernando.txt', 'a') # append mode
+#file.write("\nI am unenployed\n")
 content = file.read()
+#line = file.readline()
+#lines = file.readlines()
 print(content)
+#close the file
+file.close()
