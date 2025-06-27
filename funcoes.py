149c38fcc492ce5db8ae6dbb66cb27bed3951fa5
fretagi <fernando.retagi@gmail.com>
2025-06-27 16:41:26 +0100

    adding functions and modules

---
+++ funcoes.py
@@ -0,0 +1,15 @@
+# def greet(name):
+#     print(f"Hello, {name}!!!")
+
+
+# greet("Fernando")
+x = int(input("Please enter x: "))
+y = int(input("Please enter y: "))
+
+
+def addition(x, y):
+    return x + y
+
+
+a = addition(x, y)
+print(f"The sum of x and y is {a}")
---
+++ modu.py
@@ -0,0 +1,5 @@
+# This is an example on how to create a module
+import math_ope  # import file created earlier with name math_ope.py
+
+a = math_ope.add(5, 6)
+print(a)
